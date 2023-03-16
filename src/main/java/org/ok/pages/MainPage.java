eca2aac30e381a377531fe2d1df0f2b489827521
Kirill06344 <kirill12093@mail.ru>
2023-03-16 20:12:46 +0300

    add CorrectLoginTest

--- src/main/java/org/ok/pages/MainPage.java
+++ src/main/java/org/ok/pages/MainPage.java
@@ -1,5 +1,6 @@
 package org.ok.pages;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.$x;
@@ -8,5 +9,7 @@ public class MainPage {
 
     private final SelenideElement NAVIGATION_ELEMENT = $x("//*[@class='navigation']");
 
-
+    public void check() {
+        NAVIGATION_ELEMENT.shouldBe(Condition.visible);
+    }
 }
--- src/test/java/org/ok/tests/CorrectLoginTest.java
+++ src/test/java/org/ok/tests/CorrectLoginTest.java
@@ -2,10 +2,13 @@ package org.ok.tests;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.ok.pages.LoginPage;
+import org.ok.pages.MainPage;
 import org.ok.utils.SupportFunctions;
 import org.ok.utils.User;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CorrectLoginTest extends BaseTest{
 
     private LoginPage loginPage;
@@ -19,7 +22,10 @@ public class CorrectLoginTest extends BaseTest{
     }
 
     @Test
-    private void correctLogin() {
+    public void correctLogin() {
         SupportFunctions.login(loginPage, user);
+        loginPage.enterWithButton();
+        MainPage mainPage = new MainPage();
+        mainPage.check();
     }
 }
